c2d30731cf71e0b5c55c7aeef96839ba13004ac1
pushpa10@UTORONTO.CA <pushpa10@UTORONTO.CA@d2304edf-db78-4a20-b96f-e52c09deb206>
2016-08-01 03:19:17 +0000

    updated the contentExtractor to work if less then the five publications are present, and implemented the output formatter to format the rawcontent into the desired format for output

--- B07Assignment3/src/driver/OutputFormatter.java
+++ B07Assignment3/src/driver/OutputFormatter.java
@@ -11,6 +11,47 @@
 //*********************************************************
 package driver;
 
-public class OutputFormatter {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class OutputFormatter {
+  public String formatOneRawContent(RawContent rContent){
+    String formattedContent = "";
+    formattedContent += "-------------------------------------------------"
+        + "----------------------\n";
+    formattedContent += "1. Name of Author:\n";
+    formattedContent += "          " + rContent.getAuthorName() + "\n";
+    formattedContent += "2. Number of All Citations:\n";
+    formattedContent += "          " + rContent.getNumOfCitations() + "\n";
+    formattedContent += "3. Number of i10-index after 2009:\n";
+    formattedContent += "          " + rContent.getI10Index() + "\n";
+    formattedContent += "4. Title of the first 3 publications:\n";
+    Integer pubNumber = 1;
+    for(String publication: rContent.getPublications()){
+      formattedContent += "          " + pubNumber + "- " + publication + "\n";
+      pubNumber++;
+    }
+    formattedContent += "5. Total paper citation (first 5 papers):\n";
+    formattedContent += "          " + rContent.getTotalPaperCitations() + "\n";
+    formattedContent += "6. Total Co-Authors:\n";
+    formattedContent += "          " + rContent.getTotalCoAuthors() + "\n";
+    return formattedContent;
+  }
+  
+  public String formatAllRawContent(ArrayList<RawContent> rContents){
+    String formattedContent = "";
+    for(RawContent rContent: rContents){
+      formattedContent += formatOneRawContent(rContent);
+    }
+    formattedContent += "\n-------------------------------------------------"
+        + "----------------------\n";
+    formattedContent += "7. Co-Author list sorted (Total: "
+    + RawContent.getCoAuthors().size() + "):\n";
+    ArrayList<String> sortedCoAuthors = RawContent.getCoAuthors();
+    Collections.sort(sortedCoAuthors, String.CASE_INSENSITIVE_ORDER);
+    for(String coAuthor: sortedCoAuthors){
+      formattedContent += coAuthor + "\n";
+    }
+    return formattedContent;
+  }
 }
